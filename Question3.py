19f5ae7f761cce79c8523e79a31b873aa5727144
shitalmarsone@gmail.com <shitalmarsone@gmail.com>
2024-04-16 23:39:38 +0530

    Question3

---
+++ Question3.py
@@ -0,0 +1,9 @@
+#3] Write a program to accept three integer numbers and find its average.
+
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+num3=int(input("Enter number 3:"))
+
+Avg=int((num1+num2+num3)/3)
+
+print(f"Average of three numbers={Avg}")
